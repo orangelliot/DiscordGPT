16154d9b8a4c1b2fde78e25113bf58c01c4f16d1
elliot <elliotfisk@gmail.com>
2024-02-13 13:22:54 -0800

    minor change to write data

--- data-controller.js
+++ data-controller.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 
 module.exports = {
     appdata: JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../appdata.json'))),
-    async write_data(data){
+    write_data(data){
         this.appdata = JSON.stringify(data)
-        fs.writeFileSync(path.resolve(__dirname, '../appdata.json'), this.appdata)
     }
 }
